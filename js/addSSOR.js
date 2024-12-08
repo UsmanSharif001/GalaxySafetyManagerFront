108f4bbf87860bcf216ac9665642c3b0323c4577
thea <thsk0001@stud.kea.dk>
2024-12-08 16:10:10 +0100

    Extract form markup into separate file for sharing between views

--- js/addSSOR.js
+++ js/addSSOR.js
@@ -2,35 +2,7 @@ import {initializeMenuSSOR} from "./menuSSOR.js";
 console.log("jeg er i addSSOR")
 
 import {postObjectAsJson} from "./module.js";
-
-const addSSORHTML = () => `
-    <h2>Add New Sprinkler System</h2>
-    <form id="add-ssor-form">
-        <label>Dato:</label>
-        <input type="date" name="date"  /><br/>
-        
-        <!-- Group for Tryk under alarmventil -->
-        <fieldset>
-            <legend>Tryk over / under alarmventil (bar)</legend>
-            <label>Tryk over våd ventil:</label>
-            <input type="number" name="pressureOverWetValve"  /><br/>
-            <label>Under ventiler:</label>
-            <input type="number" name="pressureUnderValves"  /><br/>
-        </fieldset>
-
-        <label>Tryk på vandstik(bar)</label>
-        <input type="number" name="pressureOnWaterPlug" /><br/>
-        <label>Fungerende alarm til brandvæsen</label>
-        <input type="checkbox" name="alarmToFireDepartmentWorking" /><br/>
-        <label>Bemærkninger:</label>
-        <textarea name="comments"></textarea><br/>
-        <label>Underskrift</label>
-        <input type="text" name="signature" /><br>
-
-        <button type="submit">Tilføj</button>
-        <button id="cancel-add" type="button">Fortryd</button>
-    </form>
-`;
+import {addSSORHTML} from "./forms.js";
 
 
 export function initializeAddSSOR() {
