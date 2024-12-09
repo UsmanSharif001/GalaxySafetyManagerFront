fbe6b89fcb4a9957af0a2c95d93a5d9aef89587c
MuLangballe <mala0007@stud.kea.dk>
2024-12-09 09:40:51 +0100

    emergency plan, styling adjustments

--- js/Emergencyplan.js
+++ js/Emergencyplan.js
@@ -2,23 +2,33 @@ console.log("jeg er i Emergency plan");
 
 const emergencyplanHTML = () => {
     return `
+<div id="emergencyPlan">
     <h1>Brand- og evakueringsinstruks</h1>
     <h3><strong>Brandvæsenet alarmeres</strong></h3>
     <h3><strong>Telefon:</strong> 112</h3>
-    <ul>
+    <ul class="emergencyList">
         <li>Hvilket telefonnummer ringes der fra: ?</li>
         <li>Hvad er der sket?</li>
         <li>Er der tilskadekomne? - Hvor mange?</li>
         <li>Oplys alarm adressen:</li>
-            <p><i>Kulturhuset Galaksen</i></p>
-            <p><i>Bymidten 48</i></p>
-            <p><i>3500 Værløse</i></p>
     </ul>
+    <address>
+        <p><i>Kulturhuset Galaksen</i></p>
+        <p><i>Bymidten 48</i></p>
+        <p><i>3500 Værløse</i></p>
+    </address>
+
     <h3><strong>Det øvrige personale informeres om situationen</strong></h3>
-    <p>Aktiver brandtryk</p>
+    <ul class="emergencyList">
+    <li>Aktiver brandtryk</li>
+    </ul>
+
     <h3><strong>Bygningen evakueres</strong></h3>
-    <p>Luk døre!</p>
-    <p>Søg samlingssted : <i>Torvet foran biografen</i></p>
+    <ul class="emergencyList">
+    <li>Luk døre!</li>
+    <li>Søg samlingssted: <i>Torvet foran biografen</i></li>
+    </ul>
+</div>
  `
 }
 
